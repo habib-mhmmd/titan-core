89f459f4455ad48138bf86e144575090202ac6b4
Habibmuhd <muhabib10alhud@gmail.com>
2025-08-29 15:12:17 +0700

    .

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
-  companyName: 'Titan Core',
+  companyName: 'Habib Muhammad',
   siteUrl: 'https://titan-core.com',
   Socials: {
       xSocial: 'https://x.com/astrodotbuild',
@@ -14,8 +14,8 @@ export const siteConfig = {
 
 export const SEO = {
   Separator: '|',
-  SiteName: 'Titan Core',
-  defaultDescription: 'A modern, high-performance Astro JS theme',
+  SiteName: 'Portfolio',
+  //defaultDescription: 'A modern, high-performance Astro JS theme',
 };
   
 export const blogSetting = {
