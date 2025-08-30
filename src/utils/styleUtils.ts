36bfe9f287e34fea3ec63e8965b6aaa3afc6490c
Habibmuhd <muhabib10alhud@gmail.com>
2025-08-30 23:17:43 +0700

    bg

--- src/utils/styleUtils.ts
+++ src/utils/styleUtils.ts
@@ -18,7 +18,7 @@ export const getPaddingClass = ({ padding, paddingTop, paddingBottom }: PaddingO
 
 export const getBackgroundColor = (background: ThemeColor = 'base'): string => {
     const bgColors = {
-        base: '--color-background', // This should use the CSS variable --color-background set in global.css
+        base: 'bg-background-base',
         light: 'bg-background-light',
         dark: 'bg-background-dark'
     };
