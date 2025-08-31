58cb8cc86767c636ad8f403826126c71d8176a6a
Habibmuhd <muhabib10alhud@gmail.com>
2025-09-01 03:13:27 +0700

    HKJHKHK

--- src/data/menu.ts
+++ src/data/menu.ts
@@ -1,9 +1,9 @@
 // src/data/menu.ts
 
 export const headerMenu = [
-    { name: 'Theme Info', link: '/theme-info' },
+    { name: 'Projects', link: '/blog' },
     { name: 'Our Team', link: '/team' },
-    { name: 'Blog', link: '/blog' },
+    { name: 'Theme', link: '/theme-info' },
     { name: 'Style-Guide', link: '/style-guide', showArrow: false,
         children: [
             { name: 'Typography', link: '/style-guide#typography' },
